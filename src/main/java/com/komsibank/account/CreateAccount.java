cf88f05a34d416fb278b4eb00f51dac7e8954c77
abdirahman <abdixcom@gmail.com>
2023-11-24 07:15:24 +0300

    added some code

--- src/main/java/com/komsibank/account/CreateAccount.java
+++ src/main/java/com/komsibank/account/CreateAccount.java
@@ -1,7 +1,13 @@
 package com.komsibank.account;
 
+import com.komsibank.dbconnection.PostgresConnection;
+import com.komsibank.model.User;
+
 public class CreateAccount {
     public static void newAccount() {
+        System.out.println(">>>>>     CREATE NEW ACCOUNT     <<<<<");
+        User user = new User();
 
+        PostgresConnection.connect();
     }
 }
--- src/main/java/com/komsibank/dbconnection/PostgresConnection.java
+++ src/main/java/com/komsibank/dbconnection/PostgresConnection.java
@@ -1,7 +1,21 @@
 package com.komsibank.dbconnection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class PostgresConnection {
     public static void connect() {
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String user = "abdirahman";
+        String password = "Kiliman7504200235!";
 
+        // JDBC variables for opening, closing, and managing connection
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println("Connection failure.");
+            e.printStackTrace();
+        }
     }
 }

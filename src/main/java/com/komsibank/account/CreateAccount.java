6d041c3c5d3e970013a8aefc6d46e6b0ea91c642
abdirahman <abdixcom@gmail.com>
2023-11-24 14:27:33 +0300

    added method for adding a user

--- src/main/java/com/komsibank/account/CreateAccount.java
+++ src/main/java/com/komsibank/account/CreateAccount.java
@@ -3,11 +3,31 @@ package com.komsibank.account;
 import com.komsibank.dbconnection.PostgresConnection;
 import com.komsibank.model.User;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class CreateAccount {
     public static void newAccount() {
+        Scanner sc = new Scanner(System.in);
         System.out.println(">>>>>     CREATE NEW ACCOUNT     <<<<<");
-        User user = new User();
+        System.out.print("First name: ");
+        String fname = sc.nextLine();
+        System.out.print("Last name: ");
+        String lname = sc.nextLine();
+        System.out.print("Email: ");
+        String email = sc.nextLine();
+        System.out.print("Account number: ");
+        String accountNumber = sc.nextLine();
+        System.out.print("Password: ");
+        String pass = sc.nextLine();
 
-        PostgresConnection.connect();
+        PostgresConnection conn = new PostgresConnection();
+        try (Connection connection = PostgresConnection.connectToDatabase(conn.getUrl(), conn.getUser(), conn.getPassword())) {
+            PostgresConnection.insertUserData(connection, accountNumber, fname, lname, email, pass);
+        } catch (SQLException e) {
+            System.out.println("Database connection failure.");
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/komsibank/dbconnection/PostgresConnection.java
+++ src/main/java/com/komsibank/dbconnection/PostgresConnection.java
@@ -2,20 +2,41 @@ package com.komsibank.dbconnection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class PostgresConnection {
-    public static void connect() {
-        String url = "jdbc:postgresql://localhost:5432/postgres";
-        String user = "abdirahman";
-        String password = "Kiliman7504200235!";
+    private final String url = "jdbc:postgresql://localhost:5432/postgres";
+    private final String user = "abdirahman";
+    private final String password = "Kiliman7504200235!";
 
-        // JDBC variables for opening, closing, and managing connection
-        try (Connection connection = DriverManager.getConnection(url, user, password)) {
-            System.out.println("Connected to the PostgreSQL server successfully.");
-        } catch (SQLException e) {
-            System.out.println("Connection failure.");
-            e.printStackTrace();
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public static Connection connectToDatabase(String url, String user, String password) throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
+
+    public static void insertUserData(Connection connection, String accountNumber, String firstName, String lastName, String email, String password) throws SQLException {
+        String sql = "INSERT INTO users(account_number, first_name, last_name, email, password) VALUES (?, ?, ?, ?, ?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, accountNumber);
+            preparedStatement.setString(2, firstName);
+            preparedStatement.setString(3, lastName);
+            preparedStatement.setString(4, email);
+            preparedStatement.setString(5, password);
+
+            int affectedRows = preparedStatement.executeUpdate();
+            System.out.println("Rows affected: " + affectedRows);
         }
     }
 }
